c88a2a904839b46ad759bf3604d8271cf0014c42
hiromi2424 <tanaka2424@gmail.com>
2011-07-12 17:59:21 +0900

    applied base configuration

---
+++ config/site.php
@@ -0,0 +1,27 @@
+<?php
+
+$config['Site'] = array(
+	'title' => 'title',
+	'htmlTitle' => 'htmlTitle',
+	'company' => 'company',
+);
+
+$config['Role'] = array(
+	'level' => array(
+		'member' => 200,
+		'moderator' => 400,
+		'admin' => 800,
+	),
+);
+
+$config['Limits'] = array(
+	'Top' => array(
+	),
+);
+
+$config['ViewCache'] = array(
+	'Pages' => array(
+		'display' => 24 * HOUR,
+	),
+);
+
